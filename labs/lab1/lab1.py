71882bb0a17c94aeee20fcb175b3e6b5bf08899a
jesse-babson2025 <jbuchanan1@babson.edu>
2025-09-15 14:14:37 +0000

    update lab1.py and tempCodeRunnerFile.py

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -66,38 +66,38 @@ def text_analysis():
         - Number of sentences (., ?, !) 
     """
     # TODO: Get user input
-text = input("Enter some text: ")
+    text = input("Enter some text: ")
 
     # TODO: Count letters
-letters = 0
+    letters = 0
 
-for char in text:               # USE CHAR TO GO THROUGH INPUT 1 LETTER AT A TIME
-    if char.isalpha():          # .isalpha() TO DETERMINE IF THE CHARACTER IS A LETTER
-        letters += 1
+    for char in text:               # USE CHAR TO GO THROUGH INPUT 1 LETTER AT A TIME
+        if char.isalpha():          # .isalpha() TO DETERMINE IF THE CHARACTER IS A LETTER
+            letters += 1
 
     # TODO: Count words
-words = 1                       # COUNTS WORDS BY ADDING SPACES, STARTS AT SPACE "1"
+    words = 1                       # COUNTS WORDS BY ADDING SPACES, STARTS AT SPACE "1"
 
-for space in text:
+    for space in text:
     
-    if space == " ":
-        words += 1              # ADDs A SPACE TO ACCOUNT FOR LAST WORD OF SENTENCE
+        if space == " ":
+            words += 1              # ADDs A SPACE TO ACCOUNT FOR LAST WORD OF SENTENCE
     
-    else:
-        pass
+        else:
+            pass
 
     # TODO: Count sentences
 
-sentences = 0                  # Set sentences to count (start at zero)
+    sentences = 0                  # Set sentences to count (start at zero)
 
-for char in text:              # Counts sentences by looking for the characters ". ? !"
-    if char in ".?!":
-        sentences += 1
+    for char in text:              # Counts sentences by looking for the characters ". ? !"
+        if char in ".?!":
+            sentences += 1
     # TODO: Print the results
 
-print(f"Letters: {letters}")
-print(f"Words: {words}")        
-print(f"Sentences: {sentences}")    
+    print(f"Letters: {letters}")
+    print(f"Words: {words}")        
+    print(f"Sentences: {sentences}")    
 
 # Uncomment to test Part 2
 # text_analysis()
